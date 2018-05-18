783ef8617a89d137314f173c11043b886b6a3627
Hebi Li <lihebi.com@gmail.com>
2018-05-18 17:08:34 -0500

    finish download htmls from publisher website

--- mendeley/webpage.py
+++ mendeley/webpage.py
@@ -2,58 +2,108 @@
 
 import requests
 import time
-import urllib3
+import sqlite3
+import os
+import argparse
 
 
 dxdoi = 'http://dx.doi.org/'
+
+def db_get_document_ids(cursor, group_name):
+    query = ('SELECT documentId FROM RemoteDocuments'
+             ' JOIN Groups ON Groups.id=RemoteDocuments.groupId'
+             ' WHERE Groups.name="' + group_name + '"')
+    cursor.execute(query)
+    res = cursor.fetchall()
+    return list(map(lambda x: x[0], res))
+
+def db_get_groups(cursor):
+    query = "SELECT name FROM Groups"
+    cursor.execute(query)
+    res = cursor.fetchall()
+    return list(filter(lambda x: x, list(map(lambda x: x[0], res))))
+
+def db_get_doi(cursor, id):
+    query = "SELECT doi FROM Documents where id=" + str(id)
+    cursor.execute(query)
+    res = cursor.fetchone()
+    return res[0]
+
 
 def doi2link(doi):
     '''
     Make request to dx.doi.org, and follow redirects
     '''
-    r = requests.get(dxdoi + doi)
-    r.close()
-    return r.url
+    res = ''
+    # known issue
+    known = {'10.1111/1567-1364.12028':
+             'https://onlinelibrary.wiley.com/doi/full/10.1111/1567-1364.12028'}
+    if doi in known:
+        return known[doi]
+    try:
+        r = requests.get(dxdoi + doi)
+        r.close()
+        res = r.url
+    except Exception:
+        print('Error during requesting doi link')
+    return res
 
-def download_html(name, url):
-    r = requests.get(url)
-    with open(name + '.html') as f:
-        f.write(r.content)
+def full_text_url(url):
+    if url.startswith('http://aem.asm.org'):
+        return url + '.full'
+    elif url.startswith('https://onlinelibrary.wiley.com'):
+        return url.replace('abs', 'full')
+    else:
+        return url
 
+def download_html(url, filename):
+    url = full_text_url(url)
+    print('downloading ' + url + ' into ' + filename + ' ...')
+    if url.startswith('https://linkinghub.elsevier.com'):
+        print('using save_page_as to download ..')
+        os.system('save_page_as "' + url
+                  + '" --browser "firefox" --destination '
+                  + filename)
+    else:
+        r = requests.get(url)
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+
+def download_html_for_group(db_file, group_name):
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    ids = db_get_document_ids(cursor, group_name)
+    print('number of documents: ' + str(len(ids)))
+    for id in ids:
+        print('=== processing document ID: ' + str(id))
+        doi = db_get_doi(cursor, id)
+        print('querying url ..')
+        url = doi2link(doi)
+        if url:
+            filename = str(id) + '.html'
+            download_html(url, filename)
+            print('sleeping 5 sec ..')
+            time.sleep(5)
+            
 if __name__ == '__hebi__':
+    db_file = '/home/hebi/.local/share/data/Mendeley Ltd./Mendeley Desktop/lihebi.com@gmail.com@www.mendeley.com.sqlite'
+
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    groups = db_get_groups(cursor)
+    ids = db_get_document_ids(cursor, "NSF project")
+
+    download_html_for_group(db_file, "NSF project")
+    
+
     print(doi2link('10.1016/j.ymben.2015.03.003'))
     doi = '10.1016/j.ymben.2015.03.003'
     r = requests.get(dxdoi + doi)
 
     print(doi2link('10.1111/1567-1364.12028'))
-
-    r = requests.get('http://aem.asm.org/content/78/5/1611.full')
-    r = requests.get('https://onlinelibrary.wiley.com/doi/full/10.1002/bit.25683')
     with open('test.html', 'wb') as f:
         f.write(r.content)
     r.close()
-
-    token = 'D20E58439E3F7E0DA93AC68109F3114DE934C22FD44F6BF8B881601CB7EEED6D4311AF0E1F1CB0D0'
-    
-    id='S1096717606001042'
-    url = 'https://www.sciencedirect.com/sdfe/arp/pii/' + id + '/body'
-    payload = {'entitledToken': token}
-
-    url='https://www.sciencedirect.com/sdfe/arp/pii/S1096717606001042/body?entitledToken=E1257F057819F6FDD10004536A3BB09EA5E69BB57D07A218D3133668D30CC6F4692A645410721384'
-    r = requests.get(url)
-
-    full_url = url + '?entitledToken=' +token
-    res = urllib.request.urlopen(full_url)
-    
-    r = requests.get(url, params=payload)
-
-    id='S1096717606001042'
-    token='1C689D45D57E8979D321EEAA16E5F8B2C42AABA7ABF5F3B3B0844F8A58A6EC8F4B8BB0742FC554CA'
-    orig_url = 'https://linkinghub.elsevier.com/retrieve/pii/' + id
-    json_url = 'https://www.sciencedirect.com/sdfe/arp/pii/' + id + '/body?entitledToken=' + token
-    r = requests.get(orig_url)
-    jar = r.cookies
-    r = requests.get(json_url, cookies=jar)
 
     # read dois.txt
     # for each doi, get real urls, write to urls.txt
@@ -66,3 +116,11 @@ if __name__ == '__hebi__':
             time.sleep(5)
         except Exception as e:
             print('ConnectionError')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--group',
+                        help='group name', required=True)
+    parser.add_argument('db', help='database file')
+    args = parser.parse_args()
+    download_html_for_group(args.db, args.group)
