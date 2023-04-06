44ef86f005beef266df2a4f7678b68385c200671
nkwbtb <nkwbtb@outlook.com>
2023-04-06 14:48:06 -0500

    add standalone function

---
+++ step1/pdf2txt.py
@@ -0,0 +1,9 @@
+import fitz
+
+def pdf2txt(fname):
+    doc = fitz.open(fname)  # open document
+    text_list = [page.get_text() for page in doc]
+    return "\n\n".join(text_list)
+
+if __name__ == '__main__':
+    print(pdf2txt("C:/Users/NKWBTB/Desktop/2022.naacl-main.175.pdf").encode("utf-8"))
